e508742fc62cf0f987f1dc1ab58eb6bc62e58f0c
Gawsan <67488752+Gawsan@users.noreply.github.com>
2021-10-23 13:19:01 +0530

    order type dine class added

---
+++ OOPDesignPattern/src/AbstractFactory/DineIN.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package AbstractFactory;
+
+/**
+ * @author gawsa
+ *
+ */
+public class DineIN implements OrderType{
+
+	public void orderSucess() {
+		// TODO Auto-generated method stub
+		System.out.println(" Your table number is 001");
+		
+	}
+
+}
